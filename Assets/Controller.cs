187b46f97df26cf290f98378cd9047a83bc43daa
jason <jason.virak@pathmazing.com>
2018-08-02 21:08:53 +0700

    update code with button

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -65,8 +65,61 @@ public partial class Controller
         });
     }
 
+    public void OnZoomInClicked()
+    {
+        model.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+    }
+
+    public void OnZoomOutClicked()
+    {
+        model.transform.localScale -= new Vector3(0.1f, 0.1f, 0.1f);
+    }
+
+    public void OnObjectRotationChanged(Slider slider)
+    {
+        // model.transform.localRotation = new Quaternion(0.0f, slider.value, 0.0f, 0.0f);
+        model.transform.rotation = Quaternion.Euler(0f, slider.value, 0f);
+    }
+
     public void OnLightPositionChanged(Slider slider)
     {
         lightParent.rotation = Quaternion.Euler(slider.value, 0f, 0f);
     }
+
+    public void OnMovePositionClicked(string position)
+    {
+        if(position == "up")
+            model.transform.position += Vector3.up * 0.3f;
+        else if (position == "down")
+            model.transform.position -= Vector3.up * 0.3f;
+        else if (position == "left")
+            model.transform.position += Vector3.left * 0.3f;
+        else if (position == "right")
+            model.transform.position += Vector3.right * 0.3f;
+        else if (position == "backward")
+            model.transform.position -= Vector3.forward * 0.3f;
+        else if (position == "forward")
+            model.transform.position += Vector3.forward * 0.3f;
+    }
+
+    //public void OnMoveDownClicked()
+    //{
+    //    model.transform.position += Vector3.down * 10.0f;
+    //}
+    //public void OnMoveLeftClicked(string st)
+    //{
+    //    model.transform.position += Vector3.left * 10.0f;
+    //}
+    //public void OnMoveRightClicked()
+    //{
+    //    model.transform.position += Vector3.right * 10.0f;
+    //}
+    //public void OnMoveBackwardClicked()
+    //{
+    //    model.transform.position += Vector3.backward * 10.0f;
+    //}
+    //public void OnMoveForwardClicked()
+    //{
+    //    model.transform.position += Vector3.forward * 10.0f;
+    //}
 }
